5af9ef7456a25de304065639971a4b81b91bba2c
Kitaka Munyao <sylv.munyao@gmail.com>
2026-07-21 10:09:01 +0300

    Refine navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,27 +3,26 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <nav className="navbar">
-      <a href="#hero" className="navbar-logo">
-  Kitaka
-</a>
+      <div className="navbar-inner">
+        <a href="#hero" className="navbar-logo">
+          Kitaka
+        </a>
 
-      <ul className="navbar-links">
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#experience">Experience</a>
-        </li>
-        <li>
-          <a href="#skills">Skills</a>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
+        <ul className="navbar-links">
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
